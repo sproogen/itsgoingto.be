c706ee42eb267a868e0432ad3b7ead2a86319465
James Grant <sprog31@gmail.com>
2015-09-28 22:01:50 +0100

    added heroku secret

--- app/config/parameters_heroku.php
+++ app/config/parameters_heroku.php
@@ -5,3 +5,4 @@ $container->setParameter('database_port', '~');
 $container->setParameter('database_name', trim($db['path'], '/'));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
+$container->setParameter('secret', '0c165270a6aa5e54d2ab2fc7f3d09b144a4f7829');
