977780cfc0f9dfdc02157aa4d5ecc4e250fa8e1c
James Grant <sprog31@gmail.com>
2015-10-07 00:40:03 +0100

    #4 added placeholder text typing

--- app/Resources/js/itsgoingtobe.question.js
+++ app/Resources/js/itsgoingtobe.question.js
@@ -16,6 +16,35 @@ $(function() // execute once the DOM has loaded
 	      }
 	});
 
+	var placeholder = ["What film should we watch?",
+					   "What should we do this weekend?",
+					   "Who is going to win the league?",
+					   "Where should we go for drinks?"];
+	var timeOut;
+	var char = 0;
+	var placeholderNum = Math.round(Math.random() * placeholder.length);
+	alert(placeholderNum);
+	$('textarea.input-field-question').attr('placeholder', '|');
+	(function typeIt(wait) {
+	    timeOut = setTimeout(function () {
+	        char++;
+	        var type = placeholder[placeholderNum].substring(0, char);
+	        $('textarea.input-field-question').attr('placeholder', type + '|');
+
+	        if (char == placeholder[placeholderNum].length) {
+	            $('textarea.input-field-question').attr('placeholder', $('textarea.input-field-question').attr('placeholder').slice(0, -1));
+	            char = 0;
+	            placeholderNum++;
+	            if(placeholderNum >= placeholder.length) placeholderNum = 0;
+	            typeIt(1000);
+	        }else{
+	        	var humanize = Math.round(Math.random() * (150 - 30)) + 30;
+	        	typeIt(humanize);
+	        }
+
+	    }, wait);
+	}());
+
 	$('input.input-field-answer').bind('input', answerUpdated);
 	$('input.input-field-answer').bind('keydown', keyPressed);
 	$('.input-field-datepicker-trigger').click(showDatePopup);
