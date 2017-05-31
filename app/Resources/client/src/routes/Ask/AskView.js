e3a517b761dbd5321d4651ba7245470d813140fd
James Grant <sprog31@gmail.com>
2017-06-01 00:03:37 +0100

    work on sass and footer styles

--- app/Resources/client/src/routes/Ask/AskView.js
+++ app/Resources/client/src/routes/Ask/AskView.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './AskView.scss'
 
 export const AskView = () => (
-  <div className='container header-container'>
-    <div className='header center-text'>
-      <h1><span>What</span> is it going to be?</h1>
+  <div>
+    <div className='container header-container'>
+      <div className='header center-text'>
+        <h1><span>What</span> is it going to be?</h1>
+      </div>
+    </div>
+    <div className='container question-container'>
+      <span className="input input-question">
+        <label className="input-label input-label-question" htmlFor="question">Ask a question</label>
+        <textarea className="input-field input-field-question js-auto-size" rows="1" id="question" name="question" placeholder=""></textarea>
+      </span>
     </div>
   </div>
 )
