92e185e2d4d2c5e8d43879d2eb0f1d8f2aec21ed
James Grant <james.grant@simitive.com>
2017-06-08 15:03:41 +0100

    some further work on the API

--- app/Resources/client/src/store/apiActions.js
+++ app/Resources/client/src/store/apiActions.js
@@ -8,16 +8,31 @@ export const ROUTE_QUESTION = '/api/questions'
 // ------------------------------------
 export const postPoll = () => (dispatch, getState) => {
   // TODO : Change this into post with question data
-  return fetch(ROUTE_QUESTION)
-  .then((response) => response.json())
+  return fetch(ROUTE_QUESTION, {
+    method: 'POST'
+  })
   .then((response) => {
+    // TODO : Move this check into its own helper
+    if (response.status === 200) {
+      return response.json()
+    } else {
+      return response.json()
+      .then((error) => {
+        return Promise.reject({
+          status     : response.status,
+          statusText : response.statusText,
+          error
+        })
+      })
+    }
+  }).then((response) => {
     // TODO : Save the question to state
     // TODO : Redirect to the question page
     console.log(response);
     return true
   }).catch((error) => {
     // TODO : Display an error message to the user
-    console.error(error)
+    console.error('There was an error', error)
     return false
   })
 }
