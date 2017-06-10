b47d1bd9442223c581dacb923029c72ed43de4e1
James Grant <sprog31@gmail.com>
2017-06-10 14:56:17 +0100

    Added link highlight to Sharing

--- app/Resources/client/src/routes/Answer/components/Sharing/Sharing.js
+++ app/Resources/client/src/routes/Answer/components/Sharing/Sharing.js
@@ -2,26 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Sharing.scss'
 
-// TODO : Auto highlight on url
-
 class Sharing extends React.Component {
   constructor (props) {
     super(props)
   }
 
+  // Select the text contents from target of the given event
+  selectText = (event) => {
+    var doc = document,
+      text = event.target,
+      range,
+      selection
+    if (doc.body.createTextRange) {
+      range = document.body.createTextRange()
+      range.moveToElementText(text)
+      range.select()
+    } else if (window.getSelection) {
+      selection = window.getSelection()
+      range = document.createRange()
+      range.selectNodeContents(text)
+      selection.removeAllRanges()
+      selection.addRange(range)
+    }
+  }
+
+  // Generate link for sharing to twitter
   twitterLink = () =>
     'https://twitter.com/home?status=' + this.props.poll.question + ' - Answer this poll at ' + window.location.href
 
+  // Generate link for sharing to facebook
   facebookLink = () =>
     'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href
 
+  // Generate link for sharing via email
   emailLink = () =>
     'mailto:?&subject=' + this.props.poll.question + '&body=Answer%20this%20poll%20at%20' + window.location.href
 
   render = () => (
     <div>
       <h3>
-        <span className='share-link'>
+        <span
+          className='share-link'
+          onClick={ this.selectText }>
           { window.location.href.replace(/(^\w+:|^)\/\//, '') }
         </span>
         <a
