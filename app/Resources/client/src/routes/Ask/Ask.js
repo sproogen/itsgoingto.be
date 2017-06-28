c6c6e6bc96e57c7ee676ce9986dd14ebe45b5abb
James Grant <sprog31@gmail.com>
2017-06-28 22:48:51 +0100

    Added twitter headers

--- app/Resources/client/src/routes/Ask/Ask.js
+++ app/Resources/client/src/routes/Ask/Ask.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Helmet} from "react-helmet";
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { hasQuestionSelector, updatePoll, initialPoll } from '../../store/poll'
 import WordRotate from '../../components/WordRotate/WordRotate'
 import Question from './components/Question/Question'
+import ItsGoingToBeLogo from './assets/itsgoingtobe-logo.png'
 import './Ask.scss'
 
 class Ask extends React.Component {
@@ -14,6 +16,19 @@ class Ask extends React.Component {
 
   render = () => (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>It&#39;s Going To Be</title>
+
+        <meta name="description" content="Wondering where to go or what to see? Start a vote and share it with your friends or colleagues." />
+        <meta name="keywords" content="question vote poll result" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@sproogen" />
+        <meta name="twitter:title" content="It&#39;s Going To Be" />
+        <meta name="twitter:description" content="Wondering where to go or what to see? Start a vote and share it with your friends or colleagues." />
+        <meta name="twitter:image" content={ItsGoingToBeLogo} />
+      </Helmet>
       <div className={'container header-container hideable' + (this.props.hasQuestion ? ' gone' : '')}>
         <div className='header center-text'>
           <h1><WordRotate words='What,Where,When,Who' /> is it going to be?</h1>
