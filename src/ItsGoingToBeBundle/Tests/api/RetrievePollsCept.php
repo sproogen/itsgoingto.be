676fecb0e1fc589da5f5da4a11867c57abe06188
James Grant <sprog31@gmail.com>
2017-07-04 01:38:20 +0100

    Further codecepton upadtes

--- src/ItsGoingToBeBundle/Tests/api/RetrievePollsCept.php
+++ src/ItsGoingToBeBundle/Tests/api/RetrievePollsCept.php
@@ -1,8 +1,9 @@
 <?php
 
+use Codeception\Util\HttpCode;
 use ItsGoingToBeBundle\ApiTester;
 
 $I = new ApiTester($scenario);
-$I->wantTo('perform actions and see result');
+$I->wantTo('Check call return 200');
 $I->sendGET('/polls');
 $I->seeResponseCodeIs(HttpCode::OK);
