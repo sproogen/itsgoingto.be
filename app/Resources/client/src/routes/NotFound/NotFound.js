ca580dca9b2130323a8d3de713f334e73877c6a5
James Grant <sprog31@gmail.com>
2017-08-16 22:12:19 +0100

    Fix #117 hide loading screen

--- app/Resources/client/src/routes/NotFound/NotFound.js
+++ app/Resources/client/src/routes/NotFound/NotFound.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Back from 'components/Back/Back'
+import './NotFound.scss'
 
 export const NotFound = () => (
   <div>
     <Back />
     <div className='container header-container answer-header-container'>
       <div className='header center-text'>
-        <h1>404</h1>
+        <h1 className='not-found'>
+          Oh no!<br />
+          <span>It looks like we couldn't find that poll.</span>
+        </h1>
       </div>
     </div>
   </div>
