feb2a542b165d6f8b2cd5588e1dcb10b2a8e8df8
James Grant <sprog31@gmail.com>
2018-05-09 21:48:02 +0100

    Removed commented out stuff

--- app/Resources/client/src/main.js
+++ app/Resources/client/src/main.js
@@ -28,39 +28,6 @@ const render = () => {
   )
 }
 
-// Development Tools
-// ------------------------------------
-// if (__DEV__) {
-//   if (module.hot) {
-//     const renderApp = render
-//     const renderError = (error) => {
-//       const RedBox = require('redbox-react').default
-
-//       ReactDOM.render(<RedBox error={error} />, MOUNT_NODE)
-//     }
-
-//     render = () => {
-//       try {
-//         renderApp()
-//       } catch (e) {
-//         console.error(e)
-//         renderError(e)
-//       }
-//     }
-
-//     // Setup hot module replacement
-//     module.hot.accept([
-//       './components/App',
-//       './routes/index',
-//     ], () =>
-//       setImmediate(() => {
-//         ReactDOM.unmountComponentAtNode(MOUNT_NODE)
-//         render()
-//       })
-//     )
-//   }
-// }
-
 // Let's Go!
 // ------------------------------------
 if (process.env.NODE_ENV !== 'test') {
