2f5ef1ed2e7642ab54f02e839ff2957abd8530a4
James Grant <sprog31@gmail.com>
2018-05-09 01:22:09 +0100

    Added dev build and story book

--- app/Resources/client/.storybook/webpack.config.js
+++ app/Resources/client/.storybook/webpack.config.js
@@ -1,97 +1,133 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
+const autoprefixer = require('autoprefixer')
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin')
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
 const bourbon = require('bourbon')
 const bourbonNeat = require('bourbon-neat')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const project = require('../project.config')
+const getClientEnvironment = require('../config/env')
+const paths = require('../config/paths')
 
-const inProject = path.resolve.bind(path, project.basePath)
-const inProjectSrc = (file) => inProject(project.srcDir, file)
-
-const __DEV__ = project.env === 'development'
-const __TEST__ = project.env === 'test'
-const __PROD__ = project.env === 'production'
-
-const extractStyles = new ExtractTextPlugin({
-  filename: 'styles/[name].[contenthash].css',
-  allChunks: true,
-  disable: true,
-})
+const publicPath = '/'
+const env = getClientEnvironment('')
 
 module.exports = {
+  entry: [
+    require.resolve('../config/polyfills'),
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+    paths.appIndexJs,
+  ],
   resolve: {
-    modules: [
-      inProject(project.srcDir),
-      'node_modules',
-    ],
-    extensions: ['*', '.js', '.jsx', '.json'],
+    modules: [paths.appSrc, 'node_modules', paths.appNodeModules].concat(
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+    ),
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
   },
   devServer: {
     watchOptions: {
       aggregateTimeout: 300,
       poll: true
     },
+    inline: true,
+    hot: true,
   },
+  output: {
+    path: paths.appBuild,
+    pathinfo: true,
+    filename: 'static/js/bundle.js',
+    chunkFilename: 'static/js/[name].chunk.js',
+    publicPath: publicPath,
+    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+  },
+  plugins: [
+    new InterpolateHtmlPlugin(env.raw),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml,
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin(env.stringified),
+    new CaseSensitivePathsPlugin(),
+    new WatchMissingNodeModulesPlugin(paths.appNodeModules),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ],
   module: {
     rules: [
       {
-        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader',
-        options: { limit: 8192 }
+        test: /\.(js|jsx|mjs)$/,
+        include: paths.appSrc,
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: true,
+        },
       },
       {
         test: /\.(sass|scss|css)$/,
-        loader: extractStyles.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: project.sourcemaps,
-                minimize: {
-                  autoprefixer: {
-                    add: true,
-                    remove: true,
-                    browsers: ['last 2 versions'],
-                  },
-                  discardComments: {
-                    removeAll : true,
-                  },
-                  discardUnused: false,
-                  mergeIdents: false,
-                  reduceIdents: false,
-                  safe: true,
-                  sourcemap: project.sourcemaps,
-                },
-              },
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
             },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: project.sourcemaps,
-                includePaths: [
-                  bourbon.includePaths,
-                  bourbonNeat.includePaths,
-                  inProjectSrc('styles'),
-                ],
-              },
+          },
+          {
+            loader: require.resolve('sass-loader'),
+            options: {
+              importLoaders: 1,
+              includePaths: [
+                bourbon.includePaths,
+                bourbonNeat.includePaths,
+                paths.styles,
+              ],
             },
-          ],
-        })
-      }
-    ]
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9',
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.(sass|scss|css)$/, /\.ejs$/],
+        loader: require.resolve('file-loader'),
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+    ],
   },
-  plugins: [
-    extractStyles,
-    new webpack.DefinePlugin(Object.assign({
-      'process.env': { NODE_ENV: JSON.stringify(project.env) },
-      __DEV__,
-      __TEST__,
-      __PROD__,
-    }, project.globals))
-  ]
 }
