426baa7e5f3af208bd09ae79c4181657da81108c
James Grant <sprog31@gmail.com>
2018-05-09 18:45:26 +0100

    Updated storybook webpack config

--- app/Resources/client/.storybook/webpack.config.js
+++ app/Resources/client/.storybook/webpack.config.js
@@ -1,82 +1,50 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
-
 const autoprefixer = require('autoprefixer')
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
-const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin')
-const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin')
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
 const bourbon = require('bourbon')
 const bourbonNeat = require('bourbon-neat')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const getClientEnvironment = require('../config/env')
 const paths = require('../config/paths')
 
-const publicPath = '/'
 const env = getClientEnvironment('')
 
 module.exports = {
-  entry: [
-    require.resolve('../config/polyfills'),
-    require.resolve('react-dev-utils/webpackHotDevClient'),
-    paths.appIndexJs,
-  ],
   resolve: {
-    modules: [paths.appSrc, 'node_modules', paths.appNodeModules].concat(
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
-    ),
+    modules: [
+      paths.appSrc,
+      'node_modules',
+    ],
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
   },
+  entry: [
+    require.resolve('../config/polyfills'),
+  ],
   devServer: {
     watchOptions: {
       aggregateTimeout: 300,
       poll: true
     },
-    inline: true,
-    hot: true,
   },
-  output: {
-    path: paths.appBuild,
-    pathinfo: true,
-    filename: 'static/js/bundle.js',
-    chunkFilename: 'static/js/[name].chunk.js',
-    publicPath: publicPath,
-    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
-  },
-  plugins: [
-    new InterpolateHtmlPlugin(env.raw),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: paths.appHtml,
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.DefinePlugin(env.stringified),
-    new CaseSensitivePathsPlugin(),
-    new WatchMissingNodeModulesPlugin(paths.appNodeModules),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  ],
   module: {
     rules: [
       {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+        options: { limit: 8192 }
       },
       {
         test: /\.(js|jsx|mjs)$/,
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
           cacheDirectory: true,
         },
       },
@@ -104,6 +72,8 @@ module.exports = {
           {
             loader: require.resolve('postcss-loader'),
             options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
               ident: 'postcss',
               plugins: () => [
                 require('postcss-flexbugs-fixes'),
@@ -112,7 +82,7 @@ module.exports = {
                     '>1%',
                     'last 4 versions',
                     'Firefox ESR',
-                    'not ie < 9',
+                    'not ie < 9', // React doesn't support IE8 anyway
                   ],
                   flexbox: 'no-2009',
                 }),
@@ -121,13 +91,10 @@ module.exports = {
           },
         ],
       },
-      {
-        exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.(sass|scss|css)$/, /\.ejs$/],
-        loader: require.resolve('file-loader'),
-        options: {
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
-    ],
+    ]
   },
+  plugins: [
+    new webpack.DefinePlugin(env.stringified),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ]
 }
