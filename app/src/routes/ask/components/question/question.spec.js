c4a2a4ee9b3da1bde7af600229d09e93a1e6cc1a
James Grant <sprog31@gmail.com>
2018-10-31 18:26:13 +0000

    #191 Added ask route tests

--- app/src/routes/ask/components/question/question.spec.js
+++ app/src/routes/ask/components/question/question.spec.js
@@ -5,8 +5,8 @@ import EventBus from 'components/event-bus'
 import { Question } from './question'
 
 const eventBus = {
-  emit: jest.fn(),
-  addListener: jest.fn(),
+  emit        : jest.fn(),
+  addListener : jest.fn(),
 }
 
 EventBus.getEventBus = jest.fn(() => eventBus)
@@ -16,8 +16,8 @@ const props = {
     'Ask a question?',
     'Ask a different question?',
   ],
-  question: '',
-  onQuestionChange: jest.fn(),
+  question         : '',
+  onQuestionChange : jest.fn(),
 }
 
 describe('(Route) Ask', () => {
@@ -75,9 +75,9 @@ describe('(Route) Ask', () => {
         it('matches snapshot', () => {
           const wrapper = shallow(<Question {...props} />)
           wrapper.setState({
-            placeholder: 1,
-            character: 12,
-            cursor: true,
+            placeholder : 1,
+            character   : 12,
+            cursor      : true,
           })
           expect(wrapper).toMatchSnapshot()
         })
@@ -85,75 +85,3 @@ describe('(Route) Ask', () => {
     })
   })
 })
-
-//     describe('(Action) onKeyDown', () => {
-//       describe('key is KEY_UP_ARROW', () => {
-//         it('emits focus request with index - 1', () => {
-//           const wrapper = shallow(<Answer {...props} />)
-
-//           wrapper.find('input').simulate('keyDown', { keyCode: 38 })
-//           expect(eventBus.emit).toHaveBeenCalledWith('focus', -1)
-//         })
-//       })
-
-//       const emitFocusPlusOne = (keyCode) => {
-//         it('emits focus request on index + 1', () => {
-//           const wrapper = shallow(<Answer {...props} />)
-
-//           wrapper.find('input').simulate('keyDown', { keyCode })
-//           expect(eventBus.emit).toHaveBeenCalledWith('focus', 1)
-//         })
-//       }
-
-//       describe('key is KEY_DOWN_ARROW', () => {
-//         emitFocusPlusOne(40)
-//       })
-
-//       describe('key is KEY_ENTER', () => {
-//         emitFocusPlusOne(13)
-//       })
-
-//       const removeAnswerEmitFocusMinusOne = (keyCode) => {
-//         describe(('has text'), () => {
-//           let wrapper
-
-//           beforeEach(() => {
-//             wrapper = shallow(<Answer {...props} />)
-//             wrapper.find('input').simulate('keyDown', { keyCode })
-//           })
-
-//           it('should not remove answer', () => {
-//             expect(props.onRemoveAnswer).not.toHaveBeenCalled()
-//           })
-
-//           it('should not emit focus change', () => {
-//             expect(eventBus.emit).not.toHaveBeenCalled()
-//           })
-//         })
-
-//         describe(('doesn\'t have text'), () => {
-//           let wrapper
-
-//           beforeEach(() => {
-//             wrapper = shallow(<Answer {...props} text={''} />)
-//             wrapper.find('input').simulate('keyDown', { keyCode, preventDefault: () => { } })
-//           })
-
-//           it('should call prop remove answer', () => {
-//             expect(props.onRemoveAnswer).toHaveBeenCalledWith(0)
-//           })
-
-//           it('should not emit focus change', () => {
-//             expect(eventBus.emit).toHaveBeenCalledWith('focus', -1)
-//           })
-//         })
-//       }
-
-//       describe('key is KEY_BACKSPACE', () => {
-//         removeAnswerEmitFocusMinusOne(8)
-//       })
-
-//       describe('key is KEY_DELETE', () => {
-//         removeAnswerEmitFocusMinusOne(46)
-//       })
-//     })
