9af0d8dfa3616c261b32e0975bbe3572d6fc875f
James Grant <sprog31@gmail.com>
2018-11-17 21:36:12 +0000

    #205 made script executable

--- scripts/update-sonar-version.sh
+++ scripts/update-sonar-version.sh
@@ -2,20 +2,20 @@
 
 echo "Updating the SonarQube properties..."
 
-# # Get the version from package.json
-# PACKAGE_VERSION=$(cat package.json \
-#   | grep version \
-#   | head -1 \
-#   | awk -F: '{ print $2 }' \
-#   | sed 's/[",]//g' \
-#   | tr -d '[[:space:]]')
-# echo "Extracted version: ${PACKAGE_VERSION}"
+# Get the version from package.json
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+echo "Extracted version: ${PACKAGE_VERSION}"
 
-# SONAR_FILE="sonar-project.properties"
+SONAR_FILE="sonar-project.properties"
 
-# # Update the version
-# REPLACE='^sonar.projectVersion=.*$'
-# WITH="sonar.projectVersion=${PACKAGE_VERSION}"
-# sed -i "s#${REPLACE}#${WITH}#g" ${SONAR_FILE}
+# Update the version
+REPLACE='^sonar.projectVersion=.*$'
+WITH="sonar.projectVersion=${PACKAGE_VERSION}"
+sed -i "s#${REPLACE}#${WITH}#g" ${SONAR_FILE}
 
-# echo "sonar.projectVersion updated"
+echo "sonar.projectVersion updated"
