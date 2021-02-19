31e09fe1ec6dca0b248bc7c40721389b7235bfed
James Grant <sprog31@gmail.com>
2021-02-19 23:59:53 +0000

    #234 further storybook updates

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,6 +1,5 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
---
+++ app/src/stories/components/WordRotate.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import WordRotate from 'components/word-rotate'
+
+export default {
+  title: 'Components/WordRotate',
+  component: WordRotate
+}
+
+const Template = (args) =>
+  <div className='header center-text'>
+    <h1><WordRotate {...args} /></h1>
+  </div>
+
+export const Default = Template.bind({})
+Default.args = {
+  words: 'What,Where,When,Who'
+}
